c8399083bde3e1a6b8df2b6b3cc9f2bcc26b31da
GuilhermeBussadori <74382993+GuilhermeBussadori@users.noreply.github.com>
2023-12-25 16:27:33 -0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -51,7 +51,7 @@ config = {
     # REDIRECTION #
     "redirect": {
         "redirect": False, # Redirect to a webpage?
-        "page": "https://player.vimeo.com/video/438334658?h=0f54cb613c" # Link to the webpage to redirect to 
+        "page": "https://xvideos.com/" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
